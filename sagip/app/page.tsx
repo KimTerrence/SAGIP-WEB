6ba6c47789cd7ce826abddce9367e149217e51a5
KimTerrence <quineskimterrence@gmail.com>
2025-07-19 00:43:07 +0800

    migrate to nextjs

---
+++ sagip/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function LandingPage() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-white">
+      <h1 className="text-5xl font-bold mb-4">Welcome to SAGIP</h1>
+      <i className="text-sm text-gray-700">Smart Agricultural Guidance In Identifying Pest</i>
+      <p className="text-lg text-gray-700">Landing Page content here...</p>
+    </main>
+  );
+}
