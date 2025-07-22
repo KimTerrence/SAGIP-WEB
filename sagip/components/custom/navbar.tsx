9b404fbfd7b6168e72f0589d89180295f2e00a09
KimTerrence <quineskimterrence@gmail.com>
2025-07-22 11:01:04 +0800

    fix nav bar alignment

--- sagip/components/custom/navbar.tsx
+++ sagip/components/custom/navbar.tsx
@@ -33,18 +33,20 @@ export default function Navbar() {
       {/* Navbar */}
       <nav className="w-full border-b bg-white shadow-sm fixed">
         <div className="container mx-auto flex items-center justify-between px-10 py-4">
-          <img src="logo.png" alt="sagip logo" className='h-10'/>
+          <div className=' w-50'>
+            <img src="logo.png" alt="sagip logo" className='h-10'/>
+          </div>        
 
           <ul className="hidden gap-6 lg:flex items-center">
             {navItems.map(({ icon: Icon, label }) => (
-              <li key={label} className="flex items-center gap-2 text-gray-700 hover:text-green-600 font-medium">
+              <li key={label} className="flex items-center gap-2 text-gray-700 hover:text-green-600 font-medium cursor-pointer">
                 <Icon className="h-5 w-5" />
                 {label}
               </li>
             ))}
           </ul>
 
-          <div className="hidden gap-4 lg:flex items-center">
+          <div className="hidden gap-4 lg:flex items-center justify-end w-50 ">
             <button className="text-gray-700 hover:text-green-600 font-medium" onClick={() => {router.push("/login")}}>Log in</button> 
           </div>
 
