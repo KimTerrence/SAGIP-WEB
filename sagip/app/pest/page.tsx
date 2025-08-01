c01bd406b4c7eb29a8898780b68edeb50e59ab34
KimTerrence <quineskimterrence@gmail.com>
2025-08-01 22:35:26 +0800

    temporary fix

--- sagip/app/pest/page.tsx
+++ sagip/app/pest/page.tsx
@@ -65,7 +65,7 @@ export default function Pest() {
                   <p className="text-gray-600 text-sm">
                     {pest.description}
                   </p>
-                  <Badge variant={pest.crop}>{pest.crop}</Badge>
+                  <Badge variant="default">{pest.crop}</Badge>
                 </div>
               ))}
             </div>
