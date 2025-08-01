32a0918b2412ad51ced525c85dee75094b63c309
KimTerrence <quineskimterrence@gmail.com>
2025-08-01 22:29:57 +0800

    fix destroy session

--- sagip/middleware.ts
+++ sagip/middleware.ts
@@ -3,13 +3,13 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
 
-  if (!token && request.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/', request.url));
+  if (!token && request.nextUrl.pathname.startsWith('/admin')) {
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*'],
+  matcher: ['/admin/:path*'],
 };
