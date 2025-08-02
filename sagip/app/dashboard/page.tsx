7ce4f6ba9e41c91d1d2d17e3203a6d04052ea26b
KimTerrence <quineskimterrence@gmail.com>
2025-08-02 23:42:09 +0800

    added dashboard

---
+++ sagip/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import Navbar from "@/components/custom/navbar";
+
+export default function(){
+    return(
+        <>
+            <Navbar/>
+            
+            <div className="bg-white p-8">
+                <div className="grid mt-16 min-h-[82vh] w-full place-items-center bg-[url('/image/bg-hero-17.svg')] bg-center bg-contain bg-no-repeat">
+                dashboard to
+                </div>
+            </div>
+        </>
+
+    )
+}
